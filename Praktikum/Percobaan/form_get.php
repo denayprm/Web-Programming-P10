953dec4d19cd0181ebae91631567d191a7542470
denayprm <152400121+denayprm@users.noreply.github.com>
2024-05-06 09:41:26 +0700

    percobaan1

---
+++ Praktikum/Percobaan/form_get.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>FORM GET</title>
+    </head>
+    <body>
+        <form action="proses_get.php" method="GET">
+            Nama : <input type="text" name="nama"><br>
+            Alamat : <input type="text" name="Alamat"><br>
+
+            <input type="submit" value="simpan">
+        </form>
+    </body>
+</html>
